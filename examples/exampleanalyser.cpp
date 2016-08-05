7c96c74d3234b83d427fe70b28f59a79a96aa685
jkiesele <jkiesele@cern.ch>
2016-08-05 14:14:11 +0200

    some more refined example

--- examples/exampleanalyser.cpp
+++ examples/exampleanalyser.cpp
@@ -8,6 +8,15 @@
 
 
 #include "../interface/basicAnalyzer.h"
+#include "../interface/tTreeHandler.h"
+#include "../interface/tBranchHandler.h"
+
+/*
+ *
+ * In reality there should be Delphes class includes
+ *
+ */
+
 #include <iostream>
 
 class exampleanalyser: public d_ana::basicAnalyzer{
@@ -25,13 +34,23 @@ private:
 	 */
 	void analyze(size_t id){
 
+		//open file, get some branches (commented since input files missing)
+		//	d_ana::tTreeHandler tree(getSampleFile(),"treename");
+		//	d_ana::tBranchHandler<std::vector<jet> > jets=d_ana::tBranchHandler<float>(&tree,"jets");
+
+
 		std::cout << "event loop on " << getSampleFile()  <<std::endl;
 
 		if(id>0)
 			sleep(2); //just to hve different status % in this example
 		for(size_t i=0;i<1000;i++){
+
+			//	tree.setEntry(i); //associate event entry
+
 			reportStatus(i,1000);
 			usleep(4e4);
+
+			//	size_t njets=jets.content()->size(); //how to access the branch content
 		}
 
 		processEndFunction();
@@ -48,7 +67,7 @@ int main(){
 	ana.setDataSetDirectory("");
 
 	ana.readFileList("exampleinput.txt");
-//	ana.setMaxChilds(1);
+	//	ana.setMaxChilds(1);
 
 	ana.start();
 
